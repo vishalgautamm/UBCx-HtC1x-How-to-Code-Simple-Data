d9f9c05bb7ca5fc6a67a2d24d85f64f95eb5bffd
vishalgautamm <vishal.g@live.ca>
2017-04-28 12:33:04 -0400

    Completed Module 1B

---
+++ Problem Bank Solutions/Chapter 1.b/largerImage.rkt
@@ -0,0 +1,29 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname largerImage) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; Image, Image -> Boolean
+;; returns true of first image is larger (width and height is greater) than the second image
+
+(check-expect (larger-image? (rectangle 20 30 "solid" "red") (rectangle 20 30 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 20 30 "solid" "red") (rectangle 20 20 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 20 30 "solid" "red") (rectangle 20 40 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 20 30 "solid" "red") (rectangle 20 30 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 20 30 "solid" "red") (rectangle 40 30 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 20 30 "solid" "red") (rectangle 20 30 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 40 30 "solid" "red") (rectangle 20 30 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 40 30 "solid" "red") (rectangle 20 30 "solid" "green")) #f)
+(check-expect (larger-image? (rectangle 40 40 "solid" "red") (rectangle 20 30 "solid" "green")) #t)
+
+;; Stub: (define (larger-image? img1 img2) #t)
+
+;;Template
+; (define (larger-image? img1 img2)
+;  (...img1 img2))
+
+;; Function body
+(define (larger-image? img1 img2)
+  (and (> (image-height img1) (image-height img2))
+       (> (image-width img1) (image-width img2))))
+     
